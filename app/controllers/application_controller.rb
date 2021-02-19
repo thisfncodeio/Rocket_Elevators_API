6aa0b98cdf0245ffd0862da24fbae2669d673b7a
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-02-19 14:00:36 -0500

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true, with: :exception
+    protect_from_forgery prepend: true, with: :exception      
+    skip_before_action :verify_authenticity_token 
 end
