954196860b879ef039eb7e1d4a5ccb2ec8f5303f
ahsanali17 <ahsantime1@gmail.com>
2021-02-23 16:30:35 -0500

    The leads table is working data is being inserted into the database columns! Reset the database and now pushing!

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_034222) do
     t.text "project_description"
     t.string "department_in_charge_of_elevators"
     t.text "message"
-    t.date "date_of_contact_request"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
