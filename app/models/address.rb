5c67eabfe88782de63c2a7d5be8a49b9fbdc565c
Lionel <niyonlion@gmail.com>
2021-02-23 18:33:44 -0500

    tables and relations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
     belongs_to :customers
+    has_one :building
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
     belongs_to :user
     has_many :addresses
+    has_many :buildings
+    
 end
