e00cb59badba1ca86ec036d58c4ccb6323487bf1
ahsanali17 <ahsantime1@gmail.com>
2021-02-24 22:22:54 -0500

    GOT THE FILE ATTACHMENT TO WORK

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -4,8 +4,21 @@ class LeadsController < ApplicationController
     def create
         
         @lead = Lead.new(lead_params)
-        @lead.save!
         
+        # encoded_files = Base64.encode64(params[:lead][:files].read)
+        # @lead.files = encoded_files
+
+     #===================================================================================================
+     # DECLARING VARIABLES  
+     #===================================================================================================
+        attachment = params["attachment"]
+        @lead.file_name = attachment
+     
+     #===================================================================================================
+     # SAVER  
+     #===================================================================================================
+        @lead.save
+
      #===================================================================================================
      # PRINTS PARAMS INTO TERMINAL WINDOW
      #===================================================================================================
@@ -16,19 +29,26 @@ class LeadsController < ApplicationController
      #===================================================================================================
      # AFTER FORM SUBMISSION LOGIC (submission alert, redirecting, rendering, errors) 
      #===================================================================================================
-
+        if attachment == nil
+            puts "this is nil, tough luck"
+        
+            if not nil 
+                @lead.file = attachment.read
+                @lead.file_name = attachment.original_filename
+            end
+        end  
+        
         if @lead.save!
             redirect_back fallback_location: root_path, notice: "Your Request was successfully created and sent!"
         end    
     end    
-
      #===================================================================================================
      # DEFINING @lead = Lead.new(lead_params) BELOW:
      #===================================================================================================
 
      # Only allow a list of trusted parameters through.
     def lead_params
-        params.required(:leads).permit(:files, :full_name_of_contact, :company_name, :email, :phone, :project_name, :project_description, :department_in_charge_of_elevators, :message)
+        params.required(:leads).permit(:full_name_of_contact, :company_name, :email, :phone, :project_name, :project_description, :department_in_charge_of_elevators, :message, :attachment)
     end
 
 end
