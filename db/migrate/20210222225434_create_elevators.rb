55abd62b069ef36c295fddea61f985448a6cdfb8
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-02-25 02:22:03 -0500

    Faker data and admin custom done

--- db/migrate/20210222225434_create_elevators.rb
+++ db/migrate/20210222225434_create_elevators.rb
@@ -2,7 +2,7 @@ class CreateElevators < ActiveRecord::Migration[5.2]
   def change
     create_table :elevators do |t|
      
-      t.integer :serial_number
+      t.string :serial_number
       t.string :model
       t.string :building_type
       t.string :status
