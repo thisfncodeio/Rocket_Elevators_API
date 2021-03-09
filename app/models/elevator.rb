09d3c25c43f747ef2c03735be0a8fad8b1db3528
Don <don_nguyen2001@yahoo.com>
2021-03-09 11:22:54 -0800

    twilio testing phase

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,23 +1,23 @@
 class Elevator < ApplicationRecord
   enum elevator_status: [:Active, :Inactive, :Intervention]
     before_update :slack_notifier
-    # before_update :twilio_notifier
+    before_update :twilio_notifier
     belongs_to :column
    
-    # def twilio_notifier
-    #   if self.status_changed?
-    #     account_sid = "#{["account_sid"]}"
-    #     auth_token = "#{["auth_token"]}"
-    #     @client = Twilio::REST::Client.new account_sid, auth_token
+    def twilio_notifier
+      if self.status_changed?
+        account_sid = "#{ENV["account_sid"]}"
+        auth_token = "#{ENV["auth_token"]}"
+        @client = Twilio::REST::Client.new account_sid, auth_token
   
-    #     if (self.status_was != self.status) and self.status == "Intervention"
-    #       @client.api.account.messages.create(
-    #         from: "+19164725749",
-    #         to: self.column.battery.building.technical_contact_phone_for_the_building,
-    #         body: "Elevator #{self.id} with Serial Number #{self.serial_number} require maintenance.")
-    #     end
-    #   end   
-    # end
+        if (self.status_was != self.status) and self.status == "Intervention"
+          @client.api.account.messages.create(
+            from: "+19164725749",
+            to: self.column.battery.building.technical_contact_phone_for_the_building,
+            body: "Elevator #{self.id} with Serial Number #{self.serial_number} require maintenance.")
+        end
+      end   
+    end
     
     def slack_notifier
         if self.status_changed?
