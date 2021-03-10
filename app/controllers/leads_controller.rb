9e891690dc5fe25dbe0a8f1e0d34dd965c43f1ef
Tim <50148044+Tjwever@users.noreply.github.com>
2021-03-10 16:08:01 -0500

    Update leads_controller.rb

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -35,22 +35,17 @@ class LeadsController < ApplicationController
             @lead.file_name = attachment.original_filename
         end
 
-        
-        
-
-        # @lead = Lead.new(lead_params)
-        # email = params["leads[email]"]
-        from = Email.new(email: 'aakibu.dev@gmail.com')
+        @lead = Lead.new(lead_params)
+        email = params["[email]"]
+        from = Email.new(email: 'jay-t-dot-2k@gmail.com')
         to = Email.new(email: @lead.email)
         subject = 'Sending with SendGrid is Fun'
         content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-        # mail = Mail.new(from, subject, to, content)
-        mail = SendGrid::Mail.new(from, subject, to, content)
+        mail = Mail.new(from, subject, to, content)
         personalization = Personalization.new
         personalization.add_to(Email.new(email: 'jay-t-dot-2k@hotmail.com'))
         mail.add_personalization(personalization)
-        # mail.template_id = 'd-8f34084713894cdfa0ddfb0625bb19fb'
-        mail.template_id = 'd-0cf0e1cad5414f24a813b9ff8c2072c7'
+        mail.template_id = 'd-8f34084713894cdfa0ddfb0625bb19fb'
         sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
         response = sg.client.mail._('send').post(request_body: mail.to_json)
         puts response.status_code
