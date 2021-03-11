36a2260de143689359e34e125f39549b8fce0146
Don <don_nguyen2001@yahoo.com>
2021-03-11 12:26:48 -0800

    ibm_watson finally works

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -18,38 +18,38 @@ class WatsonController < ActionController::Base
         )
         text_to_speech.service_url = ENV["TEXT_TO_SPEECH_URL"]
         
-        test_file = "#{Rails.root}/app/assets/audios/audio.mp3"
-        File.open("test_file", "wb") do |audio_file|
-          response = text_to_speech.synthesize(
-                 {
-                    "text": "Greeting user #{current_user.id}. There is #{Elevator::count} elevators in #{Building::count} buildings of your 
-                    #{Customer::count} customers. Currently, #{Elevator.where(status: 'Intervention').count} elevators are not in 
-                    Running Status and are being serviced. You currently have #{Quote::count} quotes awaiting processing.
-                    You currently have #{Lead::count} leads in your contact requests. 
-                    #{Battery::count} Batteries are deployed across 
-                    #{Address.where(id: Building.select(:address_id).distinct).select(:city).distinct.count} cities",
-                    "accept": "audio/mp3",
-                    "voice": "en-GB_KateV3Voice"
-                  }
-            ).result
-            audio_file << response
-        end
-
-        # message = "Greeting user #{current_user.id}. There is #{Elevator::count} elevators in #{Building::count} buildings of your 
+        # test_file = "#{Rails.root}/app/assets/audios/audio.mp3"
+        # File.open("test_file", "wb") do |audio_file|
+        #   response = text_to_speech.synthesize(
+        #          {
+        #             "text": "Greeting user #{current_user.id}. There is #{Elevator::count} elevators in #{Building::count} buildings of your 
         #             #{Customer::count} customers. Currently, #{Elevator.where(status: 'Intervention').count} elevators are not in 
         #             Running Status and are being serviced. You currently have #{Quote::count} quotes awaiting processing.
         #             You currently have #{Lead::count} leads in your contact requests. 
         #             #{Battery::count} Batteries are deployed across 
-        #             #{Address.where(id: Building.select(:address_id).distinct).select(:city).distinct.count} cities"
+        #             #{Address.where(id: Building.select(:address_id).distinct).select(:city).distinct.count} cities",
+        #             "accept": "audio/mp3",
+        #             "voice": "en-GB_KateV3Voice"
+        #           }
+        #     ).result
+        #     audio_file << response
+        # end
 
-        # response = text_to_speech.synthesize(
-        #     text: message,
-        #     accept: "audio/mp3",
-        #     voice: "en-GB_KateV3Voice"
-        # ).result
+        message = "Greeting user #{current_user.id}. There is #{Elevator::count} elevators in #{Building::count} buildings of your 
+                    #{Customer::count} customers. Currently, #{Elevator.where(status: 'Intervention').count} elevators are not in 
+                    Running Status and are being serviced. You currently have #{Quote::count} quotes awaiting processing.
+                    You currently have #{Lead::count} leads in your contact requests. 
+                    #{Battery::count} Batteries are deployed across 
+                    #{Address.where(id: Building.select(:address_id).distinct).select(:city).distinct.count} cities"
 
-        # File.open("#{Rails.root}/../app/assets/audios/audio.mp3", "wb") do |audio_file|
-        #                 audio_file.write(response)
-        # end    
+        response = text_to_speech.synthesize(
+            text: message,
+            accept: "audio/mp3",
+            voice: "en-GB_KateV3Voice"
+        ).result
+
+        File.open("#{Rails.root}/public/outputs.mp3", "wb") do |audio_file|
+                        audio_file.write(response)
+        end    
     end
 end
