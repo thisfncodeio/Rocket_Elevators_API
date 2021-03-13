9215f3dcd2d66a5711d6328ddef79c3bfd0394d3
Abdul Akeeb <aakibu.dev@gmail.com>
2021-03-12 23:49:51 -0500

    m

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module RailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::MethodOverride
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
